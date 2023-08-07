6feb0790ac894e40d2a4bf0ce6d6fb81eb363962
zelstart <e.pomeroy95@gmail.com>
2023-08-07 18:58:09 -0400

    made some changes to JTs code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-var apiKey = fc83ec8cd64eb691a5994da1280e5c60;
-var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
-function getWeatherData(city) {
-   var apiWeatherEl = weatherUrl + city +'&appid=' + apiKey;
+$(document).ready(function () {
+
+    function getWeatherData(city) {
+        let apiKey = fc83ec8cd64eb691a5994da1280e5c60;
+        let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+        let apiWeatherEl = weatherUrl + city +'&appid=' + apiKey;
    fetch(apiWeatherEl)
    .then(function (urlResponse) {
     if (urlResponse.ok) {
@@ -10,3 +12,6 @@ function getWeatherData(city) {
     }
    })
 }
+
+
+})
