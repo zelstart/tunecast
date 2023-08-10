3bb751016883c6a6a9d09223da49127055d548a4
zelstart <e.pomeroy95@gmail.com>
2023-08-09 20:34:13 -0400

    actually unhides div now

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         })
         .then((data)=> {
           let weatherDisplay = document.getElementById('weather-display');
-          weatherDisplay.removeClass("hidden")
+          $("#weather-display").removeClass("hidden");
           weatherDisplay.innerHTML = `
           <p>City: ${data.name}</p>
           <p>Temperature: ${data.main.temp}</p>
