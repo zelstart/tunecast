c984cd832f1b1e6bf9ef03209dff822ac2be27b9
joseph <jomill0723@outlook.com>
2023-08-14 21:49:14 -0400

    location input adds yt vid

--- assets/js/geolocation-script.js
+++ assets/js/geolocation-script.js
@@ -169,6 +169,90 @@ $(document).ready(function () {
     $('#yt-desc').html("We've found some music for you based on these criteria: " + "<br><br>" + randomTemperatureWord + "<br>" + randomWindSpeedWord + "<br><br>" + "Happy listening!!");
     return searchTerm;
   }
+  function getWeatherByCityAndState(city, state) {
+    let apiKey = 'fc83ec8cd64eb691a5994da1280e5c60';
+    let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    let apiWeatherUrl = `${weatherUrl}${city}`;
+
+    if (state) {
+      apiWeatherUrl += `,${state},US`;
+    }
+
+    apiWeatherUrl += `&appid=${apiKey}&units=imperial`;
+
+  fetch(apiWeatherUrl)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.error('Error fetching weather data:', response.status);
+      }
+    })
+    .then((data)=> {
+      let weatherDisplay = document.getElementById('weather-display');
+      $("#weather-display").removeClass("hidden");
+     weatherDisplay.innerHTML = `
+      <p><span class="weather-label">City:</span> <span class="weather-value">${data.name}</span></p>
+      <p><span class="weather-label">Temperature:</span> <span class="weather-value">${data.main.temp}°F</span></p>
+      <p><span class="weather-label">Feels Like:</span> <span class="weather-value">${data.main.feels_like}°F</span></p>
+      <p><span class="weather-label">Description:</span> <span class="weather-value"> ${data.weather[0].description}</span></p>
+      <p><span class="weather-label">Wind Speed:</span> <span class="weather-value">${data.wind.speed} mph</span></p>
+      <p><span class="weather-label">Humidity:</span> <span class="weather-value">${data.main.humidity} %</p>
+      `;
+
+      let icon = data.weather[0].icon;
+      let iconUrl = `https://openweathermap.org/img/wn/${icon}.png`;
+      let weatherIcon = document.createElement('img');
+      weatherIcon.src = iconUrl;
+      weatherDisplay.appendChild(weatherIcon);
+
+      weatherDisplay.classList.remove('hidden');
+      document.getElementById('error-message').parentElement.classList.add('hidden');
+    })
+    .catch((error) => {
+      console.error('Fetch error:', error);
+      let errorMessage = document.getElementById('error-message');
+      errorMessage.textContent = 'Please enter a valid location';
+      errorMessage.parentElement.classList.remove('hidden');
+      document.getElementById('weather-display').classList.add('hidden');
+    });
+}
+  $('#getWeatherBtn').click(async function () {
+    let locationInput = $('#location-input').val().toLowerCase().trim();
+    let locationInputSplit = locationInput.split(",");
+    let city = locationInputSplit[0];
+    let state = locationInputSplit[1] ? locationInputSplit[1].trim() : null;
+  
+    let errorMessage = document.getElementById('error-message');
+  
+    if (city === '') {
+      errorMessage.textContent = 'Please enter a valid location';
+      errorMessage.parentElement.classList.remove('hidden');
+    } else {
+      errorMessage.parentElement.classList.add('hidden');
+  
+      if (state) {
+        // Get weather data for city and state
+        const weatherData = await getWeatherByCityAndState(city, state);
+  
+        // Generate search term based on weather data
+        const searchTerm = getSearchTerm(weatherData.temp, weatherData.windSpeed);
+  
+        // Search for a YouTube video using the generated search term
+        searchVideo(searchTerm);
+      } else {
+        // Get weather data for city only
+        const weatherData = await getWeatherByCityAndState(city);
+  
+        // Generate search term based on weather data
+        const searchTerm = getSearchTerm(weatherData.temp, weatherData.windSpeed);
+  
+        // Search for a YouTube video using the generated search term
+        searchVideo(searchTerm);
+      }
+    }
+  });
+  
 
   //Embeds the video by using the videoId provided by the searchVideo function
   function embedVideo(videoId) {
